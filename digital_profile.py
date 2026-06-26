16968d596085777add56399870de5e574b9629e9
407-32-Tunsinee <54721@yupparaj.ac.th>
2026-06-26 11:51:05 +0700

    digital_profile.py

--- digital_profile.py
+++ digital_profile.py
@@ -1,6 +1,6 @@
 import streamlit as
 # 1. หัวข้อใหญ่: แนะนำตัวหลักและชื่อเจ้าของเว็บ
-st.title(" * My Digital Profile")
+st.title("My Digital Profile")
 # 2. หัวข้อย่อยที่ 1: แนะนำตัว
 st header (" แนะนำตัว")
 st.write("นางสาวธัญสินี ขวัญศรีวงค์")
