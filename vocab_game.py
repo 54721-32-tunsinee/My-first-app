4d11c3869998a11123d7349062e95bd16e5e0dee
407-32-Tunsinee <54721@yupparaj.ac.th>
2026-08-21 12:00:49 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -105,7 +105,7 @@ ans3 = st.text_input(
     value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
-    "ข้อ 4: This `p _ n` people use to write or sign 🖊️ ",
+    "ข้อ 4: This `p _ n` people use to write or sign. 🖊️ ",
     value=st.session_state.ans4_val,
 
 # อัปเดตค่าล่าสุดเข้าตัวแปร
